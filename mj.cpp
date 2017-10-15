17f7b457589c038affa4b2e8d1a1b20bb82c9d1c
Christian Stigen Larsen <csl@csl.name>
2017-10-16 01:36:00 +0200

    Adds mul2 program

--- mj.cpp
+++ mj.cpp
@@ -1,6 +1,7 @@
 // Written by Christian Stigen Larsen
 // Public domain, 2017
 
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,6 +23,31 @@ void ret2_program(uint8_t* block, const size_t /*length*/)
   block[2] = 0xc3; // RETN
 }
 
+void mul2_program(uint8_t* block, const size_t /*length*/)
+{
+  /*
+   * 100000de4:	89 7d fc             	mov    %edi,-0x4(%rbp)
+   * 100000dea:	c1 e7 01             	shl    $0x1,%edi
+   * 100000ded:	89 f8                	mov    %edi,%eax
+   */
+
+  // mov edi, rbp[-4]
+  block[0] = 0x89;
+  block[1] = 0x7d;
+  block[3] = 0xfc;
+
+  // shl edi, 1
+  block[4] = 0xc1;
+  block[5] = 0xe7;
+  block[6] = 0x01;
+
+  // mox eax, edi
+  block[7] = 0x89;
+  block[8] = 0xf8;
+
+  block[9] = 0xc3; // RETN
+}
+
 int main()
 {
   const size_t pagesize = sysconf(_SC_PAGE_SIZE);
@@ -29,7 +55,7 @@ int main()
 
   uint8_t* block = static_cast<uint8_t*>(malloc(pagesize));
 
-  ret2_program(block, pagesize);
+  mul2_program(block, pagesize);
 
   if (mprotect(block, pagesize, PROT_READ | PROT_EXEC)) {
     perror("mprotect");
@@ -37,9 +63,12 @@ int main()
   }
 
   printf("calling JIT\n");
-  int (*call)(void) = reinterpret_cast<int (*)(void)>(block);
-  int8_t result = call();
+  int (*call)(int) = reinterpret_cast<int (*)(int)>(block);
+  int result = call(12);
+  assert(result == 24);
   printf("result %d\n", result);
+  for ( int i=0; i<10; ++i )
+    printf("call(%d) = %d\n", i, call(i));
 
   printf("done\n");
   return 0;
