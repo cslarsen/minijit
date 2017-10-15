002e6aa152e330fac72176d3d7949d0560455bf1
Christian Stigen Larsen <csl@csl.name>
2017-10-16 01:26:04 +0200

    Adds second program

--- mj.cpp
+++ mj.cpp
@@ -14,6 +14,14 @@ void ret_program(uint8_t* block, const size_t /*length*/)
   block[0] = 0xc3; // RETN
 }
 
+void ret2_program(uint8_t* block, const size_t /*length*/)
+{
+  // mov ax, 123
+  block[0] = 0xb0; // mov r8, imm8 (+0 = ax=
+  block[1] = 123;  // immediate value
+  block[2] = 0xc3; // RETN
+}
+
 int main()
 {
   const size_t pagesize = sysconf(_SC_PAGE_SIZE);
@@ -21,16 +29,17 @@ int main()
 
   uint8_t* block = static_cast<uint8_t*>(malloc(pagesize));
 
-  ret_program(block, pagesize);
+  ret2_program(block, pagesize);
 
   if (mprotect(block, pagesize, PROT_READ | PROT_EXEC)) {
-    perror("code");
+    perror("mprotect");
     return 1;
   }
 
   printf("calling JIT\n");
-  void (*call)(void) = reinterpret_cast<void (*)(void)>(block);
-  call();
+  int (*call)(void) = reinterpret_cast<int (*)(void)>(block);
+  int8_t result = call();
+  printf("result %d\n", result);
 
   printf("done\n");
   return 0;
